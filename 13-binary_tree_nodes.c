4fa20b33d292ce9ef7855663c8f518a9d3701cd5
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-02 11:07:05 +0300

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,5 +9,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	int leftSide = 0, rightSide = 0;
+	int has_children;
 
+	if (tree == NULL)
+		return (0);
+
+	has_children = (tree->left != NULL || tree->right != NULL);
+
+	leftSide = binary_tree_nodes(tree->left);
+	rightSide = binary_tree_nodes(tree->right);
+
+	return (leftSide + rightSide + has_children);
 }
