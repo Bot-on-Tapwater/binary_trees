91dec49a21c9320040974bbdd397ab9d243a5765
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-08-02 10:55:16 +0300

    Uncle

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,39 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: The height of the binary tree, or 0 if the tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	/* variables to store height */
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	/* NULL check for tree */
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	/* NULL check for left-child */
+	if (tree->left != NULL)
+		/* Add 1 to height and recursively call func */
+		left_height = 1 + binary_tree_height(tree->left);
+
+	/* NULL check for right-child */
+	if (tree->right != NULL)
+		/* Add 1 to height and recursively call func */
+		right_height = 1 + binary_tree_height(tree->right);
+
+	/* Select longest path */
+	return ((left_height > right_height) ? (left_height) : (right_height));
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree
  *
  * @tree: Pointer to the root node of the tree to measure the balance
@@ -9,5 +42,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+  /* int leftHeight = 0, rightHeight = 0; */
+  
+  if (tree == NULL)
+    return (0);
 
+  printf("\n\tcurrent node: %d left node height: %lu & right node height: %lu\n", tree->n,binary_tree_height(tree->left), binary_tree_height(tree->right));
+
+  return ((int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right));
+/* 
+  leftHeight = binary_tree_height(tree->left);
+  rightHeight = binary_tree_height(tree->right); */
+
+  /* return (leftHeight - rightHeight); */
 }
